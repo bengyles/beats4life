a77a4275a50bed396890eb1db6c20fa8247f6897
bengyles <bengyles@hotmail.com>
2020-07-17 18:33:30 +0200

    Added record and download

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,39 @@ import React, {Component} from 'react';
 import ExternalWaveformPlaylist from 'waveform-playlist';
 
 let playlist;
+let userMediaStream;
+let constraints = {audio: true};
+let ee;
 
 export default class WaveformPlaylist extends Component {
 
-  componentDidMount() {
-    // if (process.browser) {
+  async componentDidMount() {
+    navigator.getUserMedia = (navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
+
+    function gotStream(stream) {
+      userMediaStream = stream;
+      playlist.initRecorder(userMediaStream);
+    }
+
+    function logError(err) {
+      console.error(err);
+    }
+
+    if (navigator.mediaDevices) {
+      navigator.mediaDevices.getUserMedia(constraints)
+          .then(gotStream)
+          .catch(logError);
+    } else if (navigator.getUserMedia && 'MediaRecorder' in window) {
+      navigator.getUserMedia(
+          constraints,
+          gotStream,
+          logError,
+      );
+    }
+
     playlist = ExternalWaveformPlaylist({
       samplesPerPixel: 3000,
       mono: true,
@@ -19,50 +47,79 @@ export default class WaveformPlaylist extends Component {
         fadeColor: 'black',
       },
       controls: {
-        show: false,
+        show: true,
         width: 200,
       },
       zoomLevels: [500, 1000, 3000, 5000],
     });
 
-    playlist.load([
+    await playlist.load([
       {
         src: 'media/audio/alone.mp3',
         name: 'loop',
         gain: 1,
       },
-    ]).then(function() {
-      // can do stuff with the playlist.
-    });
+    ]);
+    // initialize the WAV exporter.
+    playlist.initExporter();
+    ee = playlist.getEventEmitter();
+    ee.on('audiorenderingfinished', this.onAudioRenderingFinished);
   }
+
+  onAudioRenderingFinished = (type, data) => {
+    if (type == 'wav') {
+      if (this.state.downloadUrl) {
+        window.URL.revokeObjectURL(this.state.downloadUrl);
+      }
+
+      let downloadUrl = window.URL.createObjectURL(data);
+
+      let a = document.createElement('a');
+      a.href = downloadUrl;
+      a.download = 'beats4life.wav';
+      a.click();
+    }
+  };
 
   play = () => {
     playlist.play();
   };
 
-  pause = () => {
-    playlist.pause();
+  record = () => {
+    if (playlist.tracks.length > 1) {
+      playlist.tracks.splice(-1, 1);
+    }
+    playlist.record();
+  };
+
+  stop = () => {
+    playlist.stop();
+  };
+
+  download = () => {
+    ee.emit('startaudiorendering', 'wav');
   };
 
   render() {
-    // }
     return (
         <div className="container">
           <Head>
             <title>Beats 4 life</title>
             <link rel="icon" href="/favicon.ico"/>
-            <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous"/>
+            <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
             <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"/>
             <link rel="stylesheet" href="/css/styles.css"/>
           </Head>
 
           <main>
-            <div style={{display: "none"}} id="playlist">
+            <div id="playlist">
 
             </div>
             <div>
               <button onClick={this.play}>Play</button>
-              <button onClick={this.pause}>Pause</button>
+              <button onClick={this.record}>Record</button>
+              <button onClick={this.stop}>Stop</button>
+              <button onClick={this.download}>Download</button>
             </div>
           </main>
         </div>
